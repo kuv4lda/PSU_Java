426e9505f6c827a44053f569206c55a3792ef2ee
kuv4lda <119787634+kuv4lda@users.noreply.github.com>
2024-10-22 09:12:45 +0500

    Update Main.java

--- lab2/Main.java
+++ lab2/Main.java
@@ -1,1 +1,55 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    Scanner in = new Scanner(System.in);
+        Random r = new Random();
+        Main m = new Main();
+        out.println("Tasks catalog:");
+        out.println("№1.4.(№1): Время.");
+        out.println("№1.5.(№2): Дом.");
+        out.println("№2.4.(№3): Сотрудники и отделы.");
+        out.println("№1.4.(№1): Время.");
+        out.println("№1.4.(№1): Время.");
+        out.println("№1.4.(№1): Время.");
+        out.println("Введите номер задачи (тот, что в скобочках): ");
+        int num = in.nextInt();
+        switch(num)
+        {
+            case(1):
+                Time time = new Time();
+                time.setSeconds(10);
+                out.println(time);
+                time.setSeconds(10000);
+                out.println(time);
+                time.setSeconds(100000);
+                out.println(time);
+                break;
+            case(2):
+                House house = new House();
+                house.setFloors(1);
+                out.println(house);
+                house.setFloors(5);
+                out.println(house);
+                house.setFloors(23);
+                out.println(house);
+                break;
+            case(3):
+                break;
+            case(4):
+                break;
+            case(5):
+                break;
+            case(6):
+                break;
+            default:
+                out.println("Такой задачи нет");
+                break;
+        }
+	}
+}
 
