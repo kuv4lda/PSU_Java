646bdc0c1a6de6a969797f7147a1258affd4e9ca
kuv4lda <119787634+kuv4lda@users.noreply.github.com>
2024-10-22 14:52:01 +0500

    Create Departament.java

---
+++ lab2/Departament.java
@@ -0,0 +1,24 @@
+public class Departament
+{
+    private String departament, boss;
+    
+    public String getDepartament()
+    {
+        return this.departament;
+    }
+    
+    public String getBoss()
+    {
+        return this.boss;
+    }
+    
+    public void setDepartament(String departament)
+    {
+        this.departament = departament;
+    }
+    
+    public void setBoss(String boss)
+    {
+        this.boss = boss;
+    }
+}
