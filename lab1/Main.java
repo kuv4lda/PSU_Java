00270195a26a85c489b3688d6623239548a38806
kuv4lda <119787634+kuv4lda@users.noreply.github.com>
2024-10-22 14:03:03 +0500

    Update Main.java

--- lab1/Main.java
+++ lab1/Main.java
@@ -251,7 +251,7 @@ class Main
                 out.println("\nРезультат: " + m.toString_Array(m.deleteNegative(Arr)));
                 break;
             default:
-                outprintln("Такой задачи нет");
+                out.println("Такой задачи нет");
         }
     }
     public String toString_Array(int[] Arr)
