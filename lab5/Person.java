156a94abf562f7e059983a29238b1c76a2929e7a
kuv4lda <119787634+kuv4lda@users.noreply.github.com>
2024-12-26 04:34:09 +0500

    Create Person.java

---
+++ lab5/Person.java
@@ -0,0 +1,55 @@
+public class Person
+{
+    private String name;
+    private Integer number;
+
+    public Person(String name, Integer number)
+    {
+        setName(name);
+        setNumber(number);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        if (name == null || name.trim().isEmpty())
+        {
+            throw new IllegalArgumentException("Имя не может быть пустым.");
+        }
+        this.name = formatName(name);
+    }
+
+    private String formatName(String name)
+    {
+        if (name == null || name.isEmpty())
+        {
+            return "";
+        }
+        return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    }
+
+
+    public Integer getNumber()
+    {
+        return number;
+    }
+
+    public void setNumber(Integer number)
+    {
+        this.number = number;
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", number=" + number +
+                '}';
+    }
+}
