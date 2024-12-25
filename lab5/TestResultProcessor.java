6b00242255114576a7a5dd497bcd35d827fc1d51
kuv4lda <119787634+kuv4lda@users.noreply.github.com>
2024-12-26 03:25:59 +0500

    Create TestResultProcessor.java

---
+++ lab5/TestResultProcessor.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestResultProcessor
+{
+    private List<Applicant> applicants;
+
+    public TestResultProcessor()
+    {
+        this.applicants = new ArrayList<>();
+    }
+
+    public void addApplicant(Applicant applicant)
+    {
+        if (applicants.size() >= 500)
+        {
+            throw new IllegalStateException("Превышено максимальное количество абитуриентов.");
+        }
+        this.applicants.add(applicant);
+    }
+
+    public List<Applicant> getAdmittedApplicants()
+    {
+        List<Applicant> admitted = new ArrayList<>();
+        for (Applicant applicant : applicants) {
+            if (applicant.isAdmitted()) {
+                admitted.add(applicant);
+            }
+        }
+        return admitted;
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return "TestResultProcessor{" + "applicants=" + applicants + '}';
+    }
+}
