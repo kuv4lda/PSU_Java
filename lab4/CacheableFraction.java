787a52fcd9d03523960690b2f6f7b499643ee939
kuv4lda <119787634+kuv4lda@users.noreply.github.com>
2024-12-26 02:55:51 +0500

    Create CacheableFraction.java

---
+++ lab4/CacheableFraction.java
@@ -0,0 +1,34 @@
+public class CacheableFraction extends Fraction
+{
+    private Double cachedDoubleValue; 
+
+    public CacheableFraction(int numerator, int denominator) // Конструктор 
+    {
+        super(numerator, denominator);
+        this.cachedDoubleValue = null;
+    }
+
+    @Override
+    public double toDouble()
+    {
+        if (cachedDoubleValue == null) // Вычисляет вещественное значение дроби
+        {                              // Если его нет, а после кэширует и возвращает
+            cachedDoubleValue = super.toDouble();
+        }
+        return cachedDoubleValue;
+    }
+
+    @Override
+    public void setNumerator(int numerator) // Сеттер (св-во) числителя
+    {
+        super.setNumerator(numerator);
+        cachedDoubleValue = null; // Сброс кэша
+    }
+
+    @Override
+    public void setDenominator(int denominator) // Сеттер (св-во) знаменателя
+    {
+        super.setDenominator(denominator);
+        cachedDoubleValue = null; // Сброс кэша
+    }
+}
