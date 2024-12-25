b9e4ec07d3b88e41f1974ed12eb9169b71ae5ca5
kuv4lda <119787634+kuv4lda@users.noreply.github.com>
2024-12-26 02:56:53 +0500

    Create Cat.java

---
+++ lab4/Cat.java
@@ -0,0 +1,37 @@
+public class Cat implements Meowable
+{
+    private String name; // Имя
+    private int meowCount = 0; // Кол-во мяу
+
+    public Cat(String name) // Конструктор
+    {
+        if (name == null || name.trim().isEmpty())
+        {
+            throw new IllegalArgumentException("Имя кота не может быть пустым.");
+        }
+        this.name = name;
+    }
+
+    public String getName() // Геттер имени
+    {
+        return name;
+    }
+    
+    public int getMeowCount() // Геттер кол-ва мяу
+    {
+        return meowCount;
+    }
+    
+    @Override
+    public void meow() // Метод для мяу кота 
+    {
+        System.out.println(name + ": мяу!");
+        meowCount++;
+    }
+
+    @Override
+    public String toString() // Метод
+    {
+        return "Кот: " + name;
+    }
+}
