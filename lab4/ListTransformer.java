f03f93e2c2a078daf0b6b65dae3719474c66ffa6
kuv4lda <119787634+kuv4lda@users.noreply.github.com>
2024-12-26 05:59:24 +0500

    Create ListTransformer.java

---
+++ lab4/ListTransformer.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTransformer 
+{
+    public static <T, P> List<P> transformList(List<T> list, Function<T, P> function)
+    {
+        if (list == null)
+        {
+             throw new IllegalArgumentException("Список не должен быть null.");
+        }
+        if (function == null)
+        {
+             throw new IllegalArgumentException("Функция не должна быть null.");
+        }
+        List<P> result = new ArrayList<>();
+        for (T item : list)
+        {
+            result.add(function.apply(item));
+        }
+        return result;
+    }
+}
