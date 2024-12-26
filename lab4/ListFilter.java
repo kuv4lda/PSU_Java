7c341ee5458c7340b95edfa196f833e0ca871ef3
kuv4lda <119787634+kuv4lda@users.noreply.github.com>
2024-12-26 06:05:50 +0500

    Create ListFilter.java

---
+++ lab4/ListFilter.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListFilter
+{
+    public static <T> List<T> filterList(List<T> list, Predicate<T> predicate)
+    {
+        if (list == null)
+        {
+            throw new IllegalArgumentException("Список не должен быть null.");
+        }
+        if(predicate == null)
+        {
+            throw new IllegalArgumentException("Предикат не должен быть null.");
+        }
+        List<T> result = new ArrayList<>();
+        for (T item : list)
+        {
+            if (predicate.test(item))
+            {
+                result.add(item);
+            }
+        }
+        return result;
+    }
+}
