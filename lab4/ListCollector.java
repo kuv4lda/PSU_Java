98bcde5332843377d31d4bbad046495ac1e0c57e
kuv4lda <119787634+kuv4lda@users.noreply.github.com>
2025-03-07 15:41:19 +0500

    Update ListCollector.java

--- lab4/ListCollector.java
+++ lab4/ListCollector.java
@@ -1,36 +1,19 @@
-import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 
-public class ListCollector
-{
+public class ListCollector {
 
-    public static <T, C extends Collection<T>, P extends Collection<C>> P collectList(List<T> list,
-                                                         CollectionCreator<P, T, C> creator,
-                                                         Collector<P, T, C> collector) {
+    public static <T, P> P collect(List<T> list,
+                                   Supplier<P> creator,
+                                   BiConsumer<P, T> collector) {
         Objects.requireNonNull(creator, "Creator не должен быть null.");
         Objects.requireNonNull(collector, "Collector не должен быть null.");
-        P collection = creator.create();
-        if(list!=null)
-        {
-            for (T item : list)
-            {
-                collector.collect(collection, item);
-            }
-        }
-        return collection;
-    }
-
-    public static <T, P extends Collection<T>> P collectSimple(List<T> list,
-                                                             CollectionCreator<P, T, T> creator,
-                                                             SimpleCollector<P, T> collector) {
-        Objects.requireNonNull(creator, "Creator не должен быть null.");
-        Objects.requireNonNull(collector, "Collector не должен быть null.");
-        P collection = creator.create();
-        if(list!=null)
-        {
+        P collection = creator.get();
+        if (list != null) {
             for (T item : list) {
-                collector.collect(collection, item);
+                collector.accept(collection, item);
             }
         }
         return collection;
